7cd830c3fdd59327c49494d8e57a29fe83b669b1
Valdinei Reis <valdineimtz@gmail.com>
2017-04-11 23:29:23 -0300

    Organizando os controllers

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php 
 
-use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use VRSFin\Application;
 use VRSFin\Plugins\DbPlugin;
@@ -23,61 +22,6 @@ $app->get('/', function(ServerRequestInterface $request) {
 	return $response;
 });
 
-$app->get('/home', function(ServerRequestInterface $request) {
-	$response = new \Zend\Diactoros\Response();
-	$response->getBody()->write("response com emmiter do diactoros");
-	return $response;
-});
-
-$app
-	->get('/category-costs', function() use($app) {
-		$view = $app->service('view.renderer');
-
-		$meuModel = new \VRSFin\Models\CategoryCost();
-		$categories = $meuModel->all();
-
-		return $view->render('category-costs/list.html.twig', [
-			'categories' => $categories
-		]);
-	}, 'category-costs.list')
-	->get('/category-costs/new', function() use($app) {
-		$view = $app->service('view.renderer');
-		return $view->render('category-costs/create.html.twig');
-	}, 'category-costs.new')
-	->post('/category-costs/store', function(ServerRequestInterface $request) use($app) {
-		$data = $request->getParsedBody();
-		\VRSFin\Models\CategoryCost::create($data);
-		return $app->route('category-costs.list');
-	}, 'category-costs.store')
-	->get('/category-costs/{id}/edit', function(ServerRequestInterface $request) use($app) {
-		$view = $app->service('view.renderer');
-		$id = $request->getAttribute('id');
-		$category = \VRSFin\Models\CategoryCost::findOrFail($id);
-		return $view->render('category-costs/edit.html.twig', [
-			'category' => $category
-		]);
-	}, 'category-costs.edit')
-	->post('/category-costs/{id}/update', function(ServerRequestInterface $request) use($app) {
-		$id = $request->getAttribute('id');
-		$category = \VRSFin\Models\CategoryCost::findOrFail($id);
-		$data = $request->getParsedBody();
-		$category->fill($data);
-		$category->save();
-		return $app->route('category-costs.list');
-	}, 'category-costs.update')
-	->get('/category-costs/{id}/show', function(ServerRequestInterface $request) use($app) {
-		$view = $app->service('view.renderer');
-		$id = $request->getAttribute('id');
-		$category = \VRSFin\Models\CategoryCost::findOrFail($id);
-		return $view->render('category-costs/show.html.twig', [
-			'category' => $category
-		]);
-	}, 'category-costs.show')
-	->get('/category-costs/{id}/delete', function(ServerRequestInterface $request) use($app) {
-		$id = $request->getAttribute('id');
-		$category = \VRSFin\Models\CategoryCost::findOrFail($id);
-		$category->delete();
-		return $app->route('category-costs.list');
-	}, 'category-costs.delete');
+require_once __DIR__ . '/../src/controllers/category-costs.php';
 
 $app->start();
