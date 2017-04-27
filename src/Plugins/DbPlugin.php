ad8762bd075105a15972cab1864f903dab08368f
Valdinei Reis <valdineimtz@gmail.com>
2017-04-27 00:33:22 -0300

    Terminando consulta de extrato

--- src/Plugins/DbPlugin.php
+++ src/Plugins/DbPlugin.php
@@ -9,6 +9,7 @@ use VRSFin\Models\BillPay;
 use VRSFin\Models\CategoryCost;
 use VRSFin\Models\User;
 use VRSFin\Repository\RepositoryFactory;
+use VRSFin\Repository\StatementRepository;
 use VRSFin\ServiceContainerInterface;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
@@ -36,6 +37,10 @@ class DbPlugin implements PluginInterface
 
         $container->addLazy('user.repository', function(ContainerInterface $container) {
             return $container->get('repository.factory')->factory(User::class);
+        });
+
+        $container->addLazy('statement.repository', function() {
+            return new StatementRepository();
         });
     }
 }
--- src/Repository/StatementRepository.php
+++ src/Repository/StatementRepository.php
@@ -3,23 +3,33 @@ declare(strict_types=1);
 
 namespace VRSFin\Repository;
 
+use Illuminate\Support\Collection;
 use VRSFin\Models\BillPay;
-use VRSFin\Models\BillReceives;
+use VRSFin\Models\BillReceive;
 
 class StatementRepository implements StatementRepositoryInterface
 {
 	public function all(string $dateStart, string $dateEnd, int $userId): array
 	{
 		$billPays = BillPay::query()
-						->selectRaw('bill_pays.*, category_costs.name as category_name')
-						->leftJoin('category_costs', 'category_costs.id', '=', 'bill_pays.category_cost_id')
-						->whereBetween('date_launch', [$dateStart, $dateEnd])
-						->where('bill_pays.user_id', $userId)
-						->get();
+			->selectRaw('bill_pays.*, category_costs.name as category_name')
+			->leftJoin('category_costs', 'category_costs.id', '=', 'bill_pays.category_cost_id')
+			->whereBetween('date_launch', [$dateStart, $dateEnd])
+			->where('bill_pays.user_id', $userId)
+			->get();
 
-		$billReceives = BillReceives::query()
-						->whereBetween('date_launch', [$dateStart, $dateEnd])
-						->where('user_id', $userId)
-						->get();
+		$billReceives = BillReceive::query()
+			->whereBetween('date_launch', [$dateStart, $dateEnd])
+			->where('user_id', $userId)
+			->get();
+
+		$collection = new Collection(array_merge_recursive($billPays->toArray(), $billReceives->toArray()));
+		$statements = $collection->sortByDesc('date_launch');
+
+		return [
+			'statements' => $statements,
+			'total_pays' => $billPays->sum('value'),
+			'total_receives' => $billReceives->sum('value')
+		];
 	}
 }
