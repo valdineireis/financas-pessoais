c00abbec7ce178fa18e79a8e7e1623434d6d0e25
Valdinei Reis <valdineimtz@gmail.com>
2017-05-03 01:12:12 -0300

    Tratamento dos parâmetros de consulta do Statements

--- src/controllers/statements.php
+++ src/controllers/statements.php
@@ -10,26 +10,7 @@ $app
             $data = $request->getQueryParams();
 
             $dateStart = dateTryParse($data['date_start'] ?? (new \DateTime())->modify('-1 month'));
-
-            /*
-            $dateStart = isset($data['date_start']) && !empty($data['date_start']) ? 
-                $data['date_start'] : (new \DateTime())->modify('-1 month');
-            $dateStart = $dateStart instanceof \DateTime ? $dateStart->format('Y-m-d') 
-            : \DateTime::createFromFormat('d/m/Y', $dateStart)->format('Y-m-d');
-            */
-
             $dateEnd = dateTryParse($data['date_end'] ?? new \DateTime());
-
-            /*
-            $dateEnd = isset($data['date_end']) && !empty($data['date_end']) ? 
-                $data['date_end'] : new \DateTime();
-            if (!($dateEnd instanceof \DateTime)) 
-                $dateEnd = \DateTime::createFromFormat('d/m/Y', $dateEnd);
-            if ($dateEnd === false) {
-                throw new \InvalidArgumentException();
-            } else {
-                $dateEnd = $dateEnd->format('Y-m-d');
-            }*/
 
             $statements = $repository->all($dateStart, $dateEnd, $auth->user()->getId());
 
